bc456617f0859072722589e4d4de0ae6ebee4083
Vasiliy <vasily.gantimurov@yandex.ru>
2023-07-25 23:51:46 +0800

    jwt and doser add

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('router.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
