eab72d980f3af1b62511acb05322ebf50b1a533c
Vasiliy <vasily.gantimurov@yandex.ru>
2023-08-13 21:50:53 +0800

    recipe creation serializer fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -168,7 +168,8 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
     def validate(self, data):
         name = self.initial_data.get('name')
         author = self.context.get('request').user
-        if Recipe.objects.filter(author=author, name=name).exists():
+        if self.method == 'POST' and Recipe.objects.filter(
+                author=author, name=name).exists():
             raise ValidationError({'name': 'Такой рецепт Вами уже добавлен'})
         ingredients = self.initial_data.get('ingredients')
         list = []
